b6365c2b19703268cee85977b0e2e10fcfba7466
zhuangshunan <zhuangshunan@baidu.com>
2021-05-19 22:09:48 +0800

    21. 合并两个有序链表

---
+++ code/21. 合并两个有序链表.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// 方法1：递归
+// var mergeTwoLists = function(l1, l2) {
+//     if (l1 === null) {
+//         return l2;
+//     }
+//     if (l2 === null) {
+//         return l1;
+//     }
+//     if (l1.val < l2.val) {
+//         l1.next = mergeTwoLists(l1.next, l2);
+//         return l1;
+//     } else {
+//         l2.next = mergeTwoLists(l1, l2.next);
+//         return l2;
+//     }
+// };
+// 方法2：迭代
+var mergeTwoLists = function(l1, l2) {
+    let preHead = new ListNode(-1, null);
+    let prev = preHead;
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            prev.next = l1;
+            l1 = l1.next;
+        } else {
+            prev.next = l2;
+            l2 = l2.next;
+        }
+        prev = prev.next;
+    }
+    prev.next = l1 === null ? l2 : l1;
+    return preHead.next;
+};
